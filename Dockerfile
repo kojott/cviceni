71d11761f147850e47a69b95e36621ffd2b0d039
Jiri George Dolejs <info@georgedolejs.com>
2024-06-26 00:34:35 +0200

    chore: Add Dockerfile and update index.html with metadata and logo image

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Update apt-get and install build-essential and ffmpeg
+RUN apt-get update && apt-get install -y \
+    build-essential
+
+# Install any needed packages specified in requirements.txt
+COPY requirements.txt /usr/src/app
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . /usr/src/app
+COPY ./logo.png /usr/local/lib/python3.9/site-packages/streamlit/static/favicon.png
+
+# Update the index
+RUN python update_index.py
+
+# Make port 8501 available to the world outside this container
+EXPOSE 8501
+
+# Run app.py when the container launches
+CMD ["streamlit", "run", "app.py"]
---
+++ update_index.py
@@ -0,0 +1,31 @@
+import re
+
+def update_html_file(file_path):
+    with open(file_path, 'r') as file:
+        html_content = file.read()
+
+    # Update the title
+    html_content = re.sub(r'<title>.*?</title>', '<title>Cviceni</title>', html_content)
+
+    # Prepare the new content to be added in the head section
+    new_head_content = """
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="Swinging Dogs s.r.o.">
+<meta property="og:title" content="">
+<meta property="og:description" content="">
+<meta property="og:url" content="https://cviceni.coininspector.pro">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://cviceni.coininspector.pro/app/static/logo.png">
+"""
+
+    # Insert the new content into the head section
+    html_content = html_content.replace('</head>', new_head_content + '</head>')
+
+    # Write the updated content back to the file
+    with open(file_path, 'w') as file:
+        file.write(html_content)
+
+# Specify the path to your HTML file
+file_path = '/usr/local/lib/python3.9/site-packages/streamlit/static/index.html'
+update_html_file(file_path)
